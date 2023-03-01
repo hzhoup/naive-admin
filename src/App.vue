8a22db110bbcee03eede5316cdd0ec1b45277d0b
hzp <hzhouping@outlook.com>
2023-03-01 21:07:09 +0800

    feat: 配置 naive 全局 hooks

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <n-config-provider :date-locale="getNaiveDateLocale" :locale="getNaiveLocale">
-    <router-view />
+  <n-config-provider :date-locale="getNaiveDateLocale" :locale="getNaiveLocale" class="h-full">
+    <NaiveProvider>
+      <router-view />
+    </NaiveProvider>
   </n-config-provider>
 </template>
 
 <script lang="ts" setup>
+import NaiveProvider from '@/components/common/NaiveProvider.vue'
 import { useLocale } from '@/locales/useLocale'
 import { NConfigProvider } from 'naive-ui'
 
---
+++ src/components/common/NaiveProvider.vue
@@ -0,0 +1,43 @@
+<template>
+  <n-loading-bar-provider>
+    <n-dialog-provider>
+      <n-notification-provider>
+        <n-message-provider>
+          <slot />
+          <NaiveProviderContent />
+        </n-message-provider>
+      </n-notification-provider>
+    </n-dialog-provider>
+  </n-loading-bar-provider>
+</template>
+
+<script lang="ts" setup>
+import {
+  NDialogProvider,
+  NLoadingBarProvider,
+  NMessageProvider,
+  NNotificationProvider,
+  useDialog,
+  useLoadingBar,
+  useMessage,
+  useNotification
+} from 'naive-ui'
+import { defineComponent, h } from 'vue'
+
+function registerNaiveTools() {
+  window.$loadingBar = useLoadingBar()
+  window.$dialog = useDialog()
+  window.$message = useMessage()
+  window.$notification = useNotification()
+}
+
+const NaiveProviderContent = defineComponent({
+  name: 'NaiveProviderContent',
+  setup() {
+    registerNaiveTools()
+  },
+  render() {
+    return h('div')
+  }
+})
+</script>
